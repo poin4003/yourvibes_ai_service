f26d51da4d6201c791f4f76601dcaf04e0796faf
Pc_Huy <pchuy4003@gmai.com>
2025-05-05 00:08:57 +0700

    singleton for config reader

--- src/utils/config_reader.py
+++ src/utils/config_reader.py
@@ -1,26 +1,18 @@
 import os
 import yaml
 
-class Server:
-    def __init__(self, config_dict):
-        self.port = config_dict.get("port", 5000)
-
-class RabbitMQConfig:
-    def __init__(self, config_dict):
-        self.url = config_dict.get("url", "amqp://guest:guest@localhost:5672/")
-        self.username = config_dict.get("username", "guest")
-        self.password = config_dict.get("password", "guest")
-        self.vhost = config_dict.get("vhost", "/")
-        self.connection_timeout = config_dict.get("connection_timeout", 10)
-        self.max_reconnect_attempts = config_dict.get("max_reconnect_attempts", 5)
-
-class CommentCensorGrpcConn:
-    def __init__(self, config_dict):
-        self.host = config_dict.get("host", "localhost")
-        self.port = config_dict.get("port", 50051)
-
 class ConfigReader:
-    def __init__(self, config_env=None):
+    _instance = None
+
+    def __new__(cls, config_env=None):
+        if cls._instance is None:
+            cls._instance = super(ConfigReader, cls).__new__(cls)
+            cls._instance._init(config_env)
+        return cls._instance
+
+    def _init(self, config_env):
+        if hasattr(self, 'config'):
+            return  
         self.config_env = config_env or os.getenv("YOURVIBES_AI_CONFIG_FILE", "dev")
         self.config_path = os.path.join("config", f"{self.config_env}.yaml")
         self.config = self.load_config()
@@ -42,7 +34,25 @@ class ConfigReader:
     def get_rabbitmq_config(self):
         rabbitmq_config = self.config.get("rabbitmq", {})
         return RabbitMQConfig(rabbitmq_config)
-    
+
     def get_comment_censor_grpc_conn_config(self):
         comment_censor_grpc_conn = self.config.get("comment_censor_grpc_conn", {})
         return CommentCensorGrpcConn(comment_censor_grpc_conn)
+
+class Server:
+    def __init__(self, config_dict):
+        self.port = config_dict.get("port", 5000)
+
+class RabbitMQConfig:
+    def __init__(self, config_dict):
+        self.url = config_dict.get("url", "amqp://guest:guest@localhost:5672/")
+        self.username = config_dict.get("username", "guest")
+        self.password = config_dict.get("password", "guest")
+        self.vhost = config_dict.get("vhost", "/")
+        self.connection_timeout = config_dict.get("connection_timeout", 10)
+        self.max_reconnect_attempts = config_dict.get("max_reconnect_attempts", 5)
+
+class CommentCensorGrpcConn:
+    def __init__(self, config_dict):
+        self.host = config_dict.get("host", "localhost")
+        self.port = config_dict.get("port", 50051)
